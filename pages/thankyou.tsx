eb9fe82bad72f202ad0b48fe832a080bd3d5bd90
AshiqRhn <71887159+AshiqRhn@users.noreply.github.com>
2022-05-24 22:09:18 -0400

    Fix Thank you page
    fixed

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -1,22 +1,11 @@
-import { Stack, Typography } from '@mui/material'
 import React from 'react'
 
 function ThankYouPage() {
   return (
-    <div style={{marginTop: '90px'}}>
-      <Stack> 
-        <Typography variant='h3' align="center" marginBottom='30px'> 
-          Thank You!
-        </Typography>
-        <Typography variant='h5'  align="center"> 
-          You've successfully sent me an email.
-        </Typography>
-        <Typography variant='body1'  align="center" marginTop='10px'> 
-          I'll reply back to you shortly. 
-        </Typography>
-
-      </Stack>
-        
+    <div style={{marginTop: '90px', textAlign: 'center', fontSize: '20px'}}>
+      <h2>Thank You! You've successfully sent me an email.</h2>
+      
+      <p> I'll reply back to you shortly.</p>      
         </div>
   )
 }
