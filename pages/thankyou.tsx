a3d41409c77509b8bf041366c269079b8be5f28f
AshiqRhn <71887159+AshiqRhn@users.noreply.github.com>
2022-05-24 22:42:33 -0400

    Update thankyou.tsx
    Trying

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -1,35 +1,15 @@
-import { Typography } from '@mui/material'
 import React from 'react'
 
-const thankyou = () => {
+const ThankYou = () => {
   return (
-    <div style={{marginTop: '90px'}}>
-      <Typography variant="h2" align="center" marginBottom={5}>
-        Thank You!
-      </Typography>
-      <Typography variant="h5" align="center"  marginBottom={1}>
-       You've successfully sent me an email.
-      </Typography>
-      <Typography variant="inherit" align="center">
-      I'll reply back to you shortly.
-      </Typography>
+    <div style={{marginTop: '90px', textAlign: 'center', fontSize: '20px'}}>
+      <h2>Thank You! You've successfully sent me an email.</h2>
+      
+      <p> I'll reply back to you shortly.</p>
      
     </div>
   )
 }
 
-export default thankyou
+export default ThankYou
 
-// import React from 'react'
-
-// function ThankYouPage() {
-//   return (
-//     <div style={{marginTop: '90px', textAlign: 'center', fontSize: '20px'}}>
-//       <h2>Thank You! You've successfully sent me an email.</h2>
-      
-//       <p> I'll reply back to you shortly.</p>      
-//         </div>
-//   )
-// }
-
-// export default ThankYouPage
