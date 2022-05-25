7304591d795c252afab54c404f0cdbdb91a5a7e4
AshiqRhn <71887159+AshiqRhn@users.noreply.github.com>
2022-05-24 22:52:22 -0400

    Update thankyou.tsx
    fix bugs

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const thankyou = () => {
   return (
-    <div>
-      thankyou
+    <div style={{marginTop: '90px', textAlign: 'center', fontSize: '20px'}}>
+     <h2>Thank You! You've successfully sent me an email.</h2>
+      
+     <p> I'll reply back to you shortly.</p> 
     </div>
   )
 }
