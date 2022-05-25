b4dea792d598b7644fc356e1b69d81d57412c2fa
AshiqRhn <71887159+AshiqRhn@users.noreply.github.com>
2022-05-24 22:25:34 -0400

    Update thankyou.tsx
    keep trying

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
-function ThankYouPage() {
+const thankyou = () => {
   return (
-    <div style={{marginTop: '90px', textAlign: 'center', fontSize: '20px'}}>
-      <h2>Thank You! You've successfully sent me an email.</h2>
-      
-      <p> I'll reply back to you shortly.</p>      
-        </div>
+    <div>thankyou</div>
   )
 }
 
-export default ThankYouPage
+export default thankyou
+
+// import React from 'react'
+
+// function ThankYouPage() {
+//   return (
+//     <div style={{marginTop: '90px', textAlign: 'center', fontSize: '20px'}}>
+//       <h2>Thank You! You've successfully sent me an email.</h2>
+      
+//       <p> I'll reply back to you shortly.</p>      
+//         </div>
+//   )
+// }
+
+// export default ThankYouPage
